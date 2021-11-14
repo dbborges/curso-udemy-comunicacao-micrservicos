6e5fd510493cbcd5df34a3ac8f0193c1d45e1a01
DESKTOP-08HB9V8\Daniel <dbborges.rj@gmail.com>
2021-11-14 20:18:55 -0300

    Adiciona estrutura inicial do auth-api

---
+++ auth-api/app.js
@@ -0,0 +1,21 @@
+import express from 'express';
+
+import * as db from './src/config/db/initialData.js';
+
+const app = express();
+const env = process.env;
+const PORT = env.PORT || 8080;
+
+db.createInitialData();
+
+app.get('/api/status', (req, res) => {
+    return res.status(200).json({
+        service: "Auth API",
+        status: "Up",
+        httpStatus: 200
+    })
+})
+
+app.listen(PORT, () => {
+    console.info(`Server stared successfuly at port ${PORT}`);
+});
