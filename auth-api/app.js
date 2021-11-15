aad9441558277bfc379327589fa5838995eec255
DESKTOP-08HB9V8\Daniel <dbborges.rj@gmail.com>
2021-11-15 10:41:43 -0300

    Implementando módulo Auth API

--- auth-api/app.js
+++ auth-api/app.js
@@ -1,12 +1,17 @@
 import express from 'express';
+import cors from 'cors';
 
 import * as db from './src/config/db/initialData.js';
+import UserRouter from "./src/modules/user/routes/UserRoutes.js";
+import checkToken from './src/config/auth/checkToken.js';
 
 const app = express();
 const env = process.env;
 const PORT = env.PORT || 8080;
 
 db.createInitialData();
+
+app.use(cors());
 
 app.get('/api/status', (req, res) => {
     return res.status(200).json({
@@ -15,6 +20,9 @@ app.get('/api/status', (req, res) => {
         httpStatus: 200
     })
 })
+
+app.use(express.json());
+app.use(UserRouter);
 
 app.listen(PORT, () => {
     console.info(`Server stared successfuly at port ${PORT}`);
---
+++ auth-api/src/config/auth/AuthException.js
@@ -0,0 +1,9 @@
+class AuthException extends Error {
+    constructor(status, message) {
+        super(message);
+        this.status = status;
+        this.message = message;
+    }
+}
+
+export default AuthException;
